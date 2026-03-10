9e71b9a5a72a37937a74557d38cabfd54bb387a1
Alex Ahumada <alex.ahumada@politicalwatch.es>
2026-03-10 16:42:46 +0100

    chore: update default env values

--- tipi_data/config.py
+++ tipi_data/config.py
@@ -2,7 +2,7 @@ from os import environ as env
 
 
 MONGO_HOST = env.get('MONGO_HOST', 'mongo')
-MONGO_DB = env.get('MONGO_DB_NAME', 'tipidb')
+MONGO_DB = env.get('MONGO_DB_NAME', 'qhlddb')
 MONGO_PORT = int(env.get('MONGO_PORT', '27017'))
-MONGO_USER = env.get('MONGO_USER', 'tipi')
-MONGO_PASSWORD = env.get('MONGO_PASSWORD', 'tipi')
+MONGO_USER = env.get('MONGO_USER', 'qhld')
+MONGO_PASSWORD = env.get('MONGO_PASSWORD', 'qhld')
